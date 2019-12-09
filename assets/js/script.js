217d0fc74bb279f0ec9d0fb8e0980735e7b6c887
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2019-12-09 21:24:44 +0000

    i added the map to script.js taking the code from earlier tutorial video for visual layout this will be amended later with my own code and i also added the box height to style.css

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+}  // this code is taken from the ealier tutorial, just using this for the moment to render map for visual layout at moment
