ac89969cbcc1084cf598ae3f2714013321a38a3f
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2019-12-14 19:20:33 +0000

    i added an event listener for each continent which is connected to an id on the destinations page. client can click button for continent and map will center on the continent, i also changed the second asian destination from bali to shanghai

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 4,
         center: {
             lat: 48.77587653040708,
             lng: 48.77587653040708
         }
     });
-} 
+
 
 var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var locations = [
 
     //north american locations
-    { lat: 40.7127281, lng: -40.7127281 }, // new york
+    { lat: 40.7127281, lng: -74.0060152}, // new york
     { lat: 34.0536909, lng: -118.2427666 }, // los angeles
     { lat: 43.653963, lng: -79.387207 }, //toronto
 
@@ -24,7 +24,7 @@ var locations = [
 
     // asian locations
     { lat: 35.6828387, lng: 139.7594549 }, //tokyo
-    { lat: -8.456018100000001, lng: 115.27038551191185 }, //bali
+    { lat: 31.2322735, lng: 121.4691749 }, //shanghai
     { lat: 22.349215549999997, lng: 114.1857978100035}, // hong kong
 
 ];
@@ -38,3 +38,25 @@ var markers = locations.map (function(location, i) {
 
 var markerCluster = new MarkerClusterer(map, markers,
     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+
+
+// this is for the onclick event when clicking for the 3 continents 
+
+google.maps.event.addDomListener(document.getElementById('northam'), 'click', function () {
+
+    map.setCenter(new google.maps.LatLng(39.88023492849345, -98.95179748535156));
+});
+
+google.maps.event.addDomListener(document.getElementById('europe'), 'click', function () {
+
+    map.setCenter(new google.maps.LatLng(51, 10));
+});
+
+google.maps.event.addDomListener(document.getElementById('asia'), 'click', function () {
+
+    map.setCenter(new google.maps.LatLng(29.84987535114661,111.6640090942383));
+});
+
+
+}
