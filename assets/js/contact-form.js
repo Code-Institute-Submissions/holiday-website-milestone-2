e767ec32bda27f43db3ce867928957a4bab76967
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2019-12-16 00:28:01 +0000

    i formatted all code so it is easier to read and well indented

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,36 +1,36 @@
-(function(){
+(function () {
     emailjs.init("user_jvqUzv5Q4vQ4CvipeoRUn");
- })();
+})();
 
 function sendForm(contactForm) {
 
-  
+
 
     var info = {
-        
-    "fullname": contactForm.fullname.value,
-    "dateofbirth": contactForm.dateofbirth.value,
-    "emailaddress": contactForm.emailaddress.value,
-    "furtherinfo": contactForm.furtherinfo.value  
+
+        "fullname": contactForm.fullname.value,
+        "dateofbirth": contactForm.dateofbirth.value,
+        "emailaddress": contactForm.emailaddress.value,
+        "furtherinfo": contactForm.furtherinfo.value
     };
 
 
     emailjs.send("gmail", "holiday_request", info)
-  
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
+
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
 
 
-        }
-    );
+            }
+        );
 
-    
+
     return false;
 
-    
+
 }
 
