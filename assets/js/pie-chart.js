bb954eed7611c4c54a6b1a6dad6037f181a70a47
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-01-01 20:31:45 +0000

    ran pie-chart.js through formatter and added comments

--- assets/js/pie-chart.js
+++ assets/js/pie-chart.js
@@ -1,19 +1,19 @@
-var ctx = document.getElementById("pieChart").getContext("2d");
-var labels = ["Recommend To A Friend", "Satisfactory", "Exceptional Service", "Unhappy"];
+var ctx = document.getElementById("pieChart").getContext("2d"); // this selects the pie chart id on reviews page and allows it to be displayed on the canvas
+var labels = ["Recommend To A Friend", "Satisfactory", "Exceptional Service", "Unhappy"]; // labels for the chart and colours below
 var colorBack = ["#51BAFA", "#EFCA08", "#129500", "#E30202"];
 
 var pieChart = new Chart(ctx, {
   type: "pie",
   data: {
     datasets: [{
-      data: [15, 20, 60, 5],
+      data: [15, 20, 60, 5], // these are the values placed in the chart as percentages 
       backgroundColor: colorBack
     }],
     labels: labels
   },
   options: {
     responsive: true,
-    legend: {
+    legend: { // this is the legend you select and where it is displayed on the chart 
       position: "bottom",
       color: "#ffffff"
     },
@@ -34,7 +34,7 @@ var pieChart = new Chart(ctx, {
           size: '10'
         },
         formatter: (value) => {
-          return value + ' %';
+          return value + ' %'; // this returns the value as a percentage
         }
       }
     }
