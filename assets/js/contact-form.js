29c4128733f39c7199b73798db830743c3711a9b
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-01-01 20:27:18 +0000

    ran contact-form.js through a code formatter and comments added

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,38 +1,37 @@
 (function () {
-    emailjs.init("user_jvqUzv5Q4vQ4CvipeoRUn");
+	emailjs.init("user_jvqUzv5Q4vQ4CvipeoRUn");
 })();
 
 function sendForm(contactForm) {
 
 
+	// this is the values that will be taken when form is filled in
+	var info = {
 
-    var info = {
-
-        "fullname": contactForm.fullname.value,
-        "dateofbirth": contactForm.dateofbirth.value,
-        "emailaddress": contactForm.emailaddress.value,
-        "furtherinfo": contactForm.furtherinfo.value
-    };
-
-
-    emailjs.send("gmail", "holiday_request", info)
-
-        .then(
-            $("#btn1").click(function () {
-
-                $('#myModal').modal()
-
-            },
-                function (error) {
-                    alert("FAILED", error);
+		"fullname": contactForm.fullname.value,
+		"dateofbirth": contactForm.dateofbirth.value,
+		"emailaddress": contactForm.emailaddress.value,
+		"furtherinfo": contactForm.furtherinfo.value
+	};
 
 
-                })
-        );
+	emailjs.send("gmail", "holiday_request", info)
+		// this will display a modal upon completion of the form
+		.then(
+			$("#btn1").click(function () {
+
+					$('#myModal').modal()
+
+				}, // this will display an error if the form is not completed
+				function (error) {
+					alert("FAILED", error);
 
 
-    return false;
+				})
+		);
+
+
+	return false;
 
 
 }
-
